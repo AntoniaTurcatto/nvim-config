bccd925612f66cbda55bc337f4500d8f7c68420d
antoniaturcatto <antoniaturcatto29@gmail.com>
2026-03-08 05:39:22 -0300

    first commit

---
+++ lua/config/keybinds.lua
@@ -0,0 +1,7 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>so", vim.cmd.so)
+vim.keymap.set("n", "<leader>wq", ':wq<CR>')
+
+vim.keymap.set("n", "<leader>w", vim.cmd.w)
+
