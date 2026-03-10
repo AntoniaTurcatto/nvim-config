813564f1a077939881128ed9f9263df44831e9d8
antoniaturcatto <antoniaturcatto29@gmail.com>
2026-03-09 21:24:08 -0300

    indent

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,11 +5,4 @@ vim.opt.shiftwidth = 2
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
 
-vim.cmd [[
-  highlight Normal guibg=none
-  highlight NonText guibg=none
-  highlight Normal ctermbg=none
-  highlight NonText ctermbg=none
-]]
